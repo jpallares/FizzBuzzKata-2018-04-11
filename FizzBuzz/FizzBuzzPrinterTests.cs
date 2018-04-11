c18c1a33d3201ef41fd670dcfe149620854012d8
Abe Lourenco <alourenco@vistaprint.com>
2018-04-11 14:09:34 +0200

    Setup of new class test FizzBuzzPrinterTests

---
+++ FizzBuzz/FizzBuzzPrinterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzPrinterTests
+    {
+        private Mock<ILinePrinter> mockPrinter;
+        private FizzBuzzPrinter fizzBuzzPrinter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockPrinter = new Mock<ILinePrinter>();
+            fizzBuzzPrinter = new FizzBuzzPrinter(mockPrinter);
+        }
+
+        [Test]
+        public void Prints_1()
+        {
+            mockPrinter.Verify(m => m.WriteLine("1"));
+        }
+
+        private interface ILinePrinter
+        {
+            void WriteLine(string output);
+        }
+    }
+}
